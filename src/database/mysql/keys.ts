76d21144e2734ce90a9b2add5fd0edb00458118e
Cristhian Reyes <cristian5402@gmail.com>
2020-07-30 19:00:36 -0500

    cambio de base de datos remota

--- src/database/mysql/keys.ts
+++ src/database/mysql/keys.ts
@@ -7,12 +7,22 @@
     }
 }; */
 
-const keys  = {
+/* const keys  = {
     baseDatos: {
         host: 'sql10.freemysqlhosting.net',
         user: 'sql10356593',
         password: 'qDXlYUrPu6',
         database: 'sql10356593',
+        port: 3306
+    }
+}; */
+
+const keys  = {
+    baseDatos: {
+        host: 'db4free.net',
+        user: 'kiba5402',
+        password: 'skullskate',
+        database: 'empleados_vortex',
         port: 3306
     }
 };
